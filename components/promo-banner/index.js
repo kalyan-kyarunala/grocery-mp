d44509120261d391b2fbd456d22c2f5486b09074
kalyan-kyarunala <kalyan.k@skitech.ai>
2026-07-01 22:30:54 +0800

    home screen updated

--- components/promo-banner/index.js
+++ components/promo-banner/index.js
@@ -1,8 +1,4 @@
 Component({
   props: { promo: {}, onTap: function () {} },
-  data: {
-    // bunting flag colours (alternating), rendered as CSS triangles
-    flags: ['#ffffff', '#34D27B', '#93B4F5', '#ffffff', '#34D27B', '#93B4F5', '#ffffff', '#34D27B', '#93B4F5']
-  },
   methods: { tap() { this.props.onTap(this.props.promo); } }
 });
